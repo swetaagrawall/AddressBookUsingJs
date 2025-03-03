589b131e5b052322d6acfc23edc8b057c2358362
sweta <swetaarawal972@gmail.com>
2025-03-03 16:13:44 +0530

    [Sweta Agrawal]Add. Added contact validation

--- src/addressBook.js
+++ src/addressBook.js
@@ -12,4 +12,15 @@ class Contact {
   }
   
   let addressBook=[];
-  
+  function validateContact(contact) {
+    const nameRegex = /^[A-Z][a-z]{2,}/;
+    const addressRegex = /^.{4,}/;
+    const zipRegex = /^\d{5}$/;
+    const phoneRegex = /^\d{10}$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    
+    if (!nameRegex.test(contact.firstName) || !nameRegex.test(contact.lastName)) return false;
+    if (!addressRegex.test(contact.address) || !addressRegex.test(contact.city) || !addressRegex.test(contact.state)) return false;
+    if (!zipRegex.test(contact.zip) || !phoneRegex.test(contact.phone) || !emailRegex.test(contact.email)) return false;
+    return true;
+  }
