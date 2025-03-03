b63d4dc8cd593b7427672adefce373ae97d16531
sweta <swetaarawal972@gmail.com>
2025-03-03 16:08:34 +0530

    [Sweta Agrawal]Add. Created AddressBook and Contact class

---
+++ src/addressBook.js
@@ -0,0 +1,15 @@
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.address = address;
+      this.city = city;
+      this.state = state;
+      this.zip = zip;
+      this.phone = phone;
+      this.email = email;
+    }
+  }
+  
+  let addressBook=[];
+  
